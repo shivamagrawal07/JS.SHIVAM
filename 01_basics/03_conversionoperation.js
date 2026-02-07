9569842bf1e576b5e4bd6fa01900dfc9c632319f
shivamagrawal07 <shivamagrawal041@gmail.com>
2026-02-07 10:00:38 +0000

    container config done

---
+++ 01_basics/03_conversionoperation.js
@@ -0,0 +1,67 @@
+letscore = null
+letscore = null
+letscore = null
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1 , false =>0
+
+let isLoggedIn = 0
+
+let booleanIsLoggedIn = Boolean(isLoggedIn) 
+// console.log(booleanIsLoggedIn);
+
+// 1 => true
+// 0 => false
+// "" => false
+// "shivam" => true
+// null => false
+// undefined => false
+// " " => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+// console.log(stringNumber)
+// console.log(typeof stringNumber);
+
+// *********************************operations in JavaScript**************************
+
+let value =3
+let negvalue = -value
+// console.log(negvalue)
+
+
+
+let str1 = "Hello"
+let str2 = " Shivam"
+
+let str3 = str1 + str2
+// console.log(str3)
+
+
+// console.log("1" + 2) // "12"   
+// console.log(1 + "2") // "12"
+// console.log(1 + 2)   // 3 
+// console.log(1 + 2 + "3") // "33"
+// console.log("1" + 2 + 3) // "123"
+
+
+// console.log(+true)
+// console.log(+"")
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 3
+
+let gamecounter = 100
+gamecounter++;
+console.log(gamecounter);
