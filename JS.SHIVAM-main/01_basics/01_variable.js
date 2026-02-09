474bca75bd02368f55833c7e3658f5e6b7ab16aa
shivamagrawal07 <shivamagrawal041@gmail.com>
2026-02-09 19:16:12 +0530

    .js

---
+++ JS.SHIVAM-main/01_basics/01_variable.js
@@ -0,0 +1,13 @@
+const accountId = 123456
+let accountEmail ="shivamagrawal@google.com"
+var accountPassword = "12345"
+accountCity ="jaipur"
+
+
+console.log(accountId)
+
+/*
+prefer not to use var
+*/
+
+console.table([accountId, accountEmail, accountPassword, accountCity])
---
+++ JS.SHIVAM-main/02_basics/04_object.js
@@ -0,0 +1,75 @@
+// const tinderuser = new Object()
+const tinderuser = {}
+
+tinderuser.id = "123abc"
+tinderuser.name = "shiva"
+tinderuser.isLoggedIn = false
+
+// console.log(tinderuser);
+
+const regularuser = {
+    email: "shivamagrawal041@gmail.com",
+    fullname: {
+        userfullname: {
+            firstname: "shivam",
+            lastname: "agrawal"
+        }
+    }
+}
+
+// console.log(regularuser.fullname);
+
+const obj1 = {1: "a", 2: "b"}
+const obj2 = {3: "c", 4: "d"}   
+
+// const obj3 = Object.assign({}, obj1, obj2)
+const obj3 = {...obj1, ...obj2}
+// console.log(obj3);
+
+const user = [
+    {
+    id: 1,
+    name: "shivam",
+    age: 22,  
+    },
+    {
+    id: 2,
+    name: "agrawal",
+    age: 21,  
+    },
+]
+user[1].name
+// console.log(tinderuser);
+
+// console.log(Object.keys(tinderuser));
+// console.log(Object.values(tinderuser));
+// console.log(Object.entries(tinderuser));   
+
+// console.log(tinderuser.hasOwnProperty("name"));
+
+
+
+
+const course = {
+    name: "js in hindi",
+    price: 999,
+    courseInstructor: "shivam agrawal"
+}   
+
+// course.courseInstructor
+
+const {courseInstructor : instructor} = course
+
+console.log(instructor);
+
+// {
+//     name: "shivam agrawal",
+//     price: 999,
+//     coursename: "js in hindi" 
+// }
+
+[
+    {},
+    {},
+    {}
+]
