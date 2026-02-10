c552bd1438638682e4ca561fde5d8504ab7dbd75
shivamagrawal07 <shivamagrawal041@gmail.com>
2026-02-10 21:17:34 +0530

    Add Immediately Invoked Function Expressions (IIFE) examples to 04_life.js

---
+++ 03_basics/04_life.js
@@ -0,0 +1,11 @@
+//  Immediately invoked function Expressions (IIFE)
+
+(function chai (){
+    // named IIFE
+    console.log(`DB CONNECTED`);
+})();
+
+(  (name) =>  {
+    console.log(`DB CONNECTED TWO ${name}`);
+} )(`shivam`)
+
