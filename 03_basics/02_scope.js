7719f02634493a529b4d5529c4da15b7b67c2acc
shivamagrawal07 <shivamagrawal041@gmail.com>
2026-02-10 18:06:21 +0530

    Refactor scope.js by commenting out console logs and add new arrow.js file

--- 03_basics/02_scope.js
+++ 03_basics/02_scope.js
@@ -3,9 +3,49 @@ let a =100; //function scope
 if(true){                
     let a =10;             //block scope
     const b =20;
-    console.log("INNER: ", a);
+    // console.log("INNER: ", a);
 }
 
-console.log(a);
+// console.log(a);
 // console.log(b);
 // console.log(c);
+
+
+
+function one (){
+    const username = "shivam";
+
+    function two(){
+        const website = "www.google.com";
+        console.log(username)
+    }
+    // console.log(website);
+    two()
+
+}
+// one()
+
+if (true){
+    const username ="shivam"
+    if (username === "shivam"){
+        const website = "youtube";
+        // console.log(website + username);
+    }
+    // console.log("website");
+}
+// console.log("username");
+
+// +++++++++++++++++++++++++ interesting ++++++++++++++++++++++++++++++++++++++++
+
+function addone(num){
+    return num+1
+}
+
+console.log(addone(5))
+
+
+const addtwo = function(num){
+    return num+2
+}
+
+console.log(addtwo(5))
