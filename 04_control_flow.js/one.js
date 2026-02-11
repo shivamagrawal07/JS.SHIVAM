b0feb5a385798d53ff1648eb11dbe659cd24df27
shivamagrawal07 <shivamagrawal041@gmail.com>
2026-02-11 21:38:29 +0530

    Added Control Flow JS file

---
+++ 04_control_flow.js/one.js
@@ -0,0 +1,20 @@
+// if
+
+const isuserloggedin = true
+const temperature = 41
+
+// if(temperature === 41){
+//     console.log("less tha 50");
+// }else{
+// console.log("temparature is greater than 50")
+// }
+
+// // <, >, >=, <=, ==, !=, ===, !==
+
+// const score = 200
+
+// if (score > 100){
+//     const power ="fly"
+//     console.log(`user power: ${power}`);
+// }
+//  console.log(`user power: ${power}`);
