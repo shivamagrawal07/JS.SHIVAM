6ab9588d2591b955615faed51cedc7f4b2fd1859
shivamagrawal07 <shivamagrawal041@gmail.com>
2026-03-02 17:57:24 +0530

    js

---
+++ 10_classes_and_oop/notes.md
@@ -0,0 +1,23 @@
+// javascript and classes
+
+// oop
+
+## object
+- collection of prperties and method
+- toLowercase
+
+## Why use oop
+
+## parst of oop
+object literal
+
+- constructor function
+- prototypes
+- classes
+- Instances(new, this)
+
+## 4 pillars
+Abstraction
+Encapsulation   // to rap the data  
+Inheritance
+Polymorphism
---
+++ 10_classes_and_oop/object.js
@@ -0,0 +1,26 @@
+function multipleBy5(num){
+    return num*5    
+}
+
+multipleBy5.power = 2
+
+console.log(multipleBy5(5));
+console.log(multipleBy5.power);
+console.log(multipleBy5.prototype);
+
+function createuser (username, score){
+    this.username = username
+    this.score = score
+}
+
+createuser.prototype.increment = function(){
+    this.score++
+}
+createuser.prototype.printMe = function(){
+    console.log(`price is ${this.score}`);
+}
+
+const chai = new createuser("chai", 25)
+const tea = createuser("tea", 250)
+
+chai.printMe()
