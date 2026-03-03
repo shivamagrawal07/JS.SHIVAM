06fd0060f74d878e7c07233c301c113d82d65667
shivamagrawal07 <shivamagrawal041@gmail.com>
2026-03-03 11:10:23 +0530

    js

---
+++ 10_classes_and_oop/inheritance.js
@@ -0,0 +1,23 @@
+class User {
+}
+
+class Teacher extends User{
+    constructor(username, email, password){
+        super(username)
+        this.email = email
+        this.password = password
+    }
+
+    addCourse(){
+        console.log(`A new course was added by ${this.username}`);
+    }
+}
+
+const chai = new Teacher("chai", "chai@teacher.com", "123")
+
+chai.logMe()
+const masalaChai = new User("masalaChai")
+
+masalaChai.logMe()
+
+console.log(chai instanceof User);
